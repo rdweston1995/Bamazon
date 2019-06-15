8b92358a8e157392f3cc17af0e46c8be9dc9dfdc
Robert <46203504+rdweston1995@users.noreply.github.com>
2019-06-15 11:51:20 -0700

    Finished add inventory module of manager file

--- bamazonManager.js
+++ bamazonManager.js
@@ -9,40 +9,48 @@ var connection = mysql.createConnection({
     password: 'Robobutt23!',
     database: 'bamazon' 
  });
+ menu();
 //  * List a set of menu options:
 //    * View Products for Sale 
 //    * View Low Inventory   
 //    * Add to Inventory
 //    * Add New Product
-inquirer.prompt([
-    {
-        type:"list",
-        message:"Menu Options:",
-        choices:['View Products for Sale', 'View Low Inventory', 'Add to Inventory', 'Add New Product'],
-        name: "input"
-    }
-]).then(function(response){
-    console.log(response.input);
+function menu(){
+    inquirer.prompt([
+        {
+            type:"list",
+            message:"Menu Options:",
+            choices:['View Products for Sale', 'View Low Inventory', 'Add to Inventory', 'Add New Product', 'Exit'],
+            name: "input"
+        }
+    ]).then(function(response){
+        //console.log(response.input);
+    
+        //Switch to get the user response and call the appropriate 
+        switch(response.input){
+            case 'View Products for Sale':
+                products();
+                break;
+            case 'View Low Inventory':
+                lowInventory();
+                break;
+            case 'Add to Inventory':
+                addInventory();
+                break;
+            case 'Add New Product':
+                addProduct();
+                break;
+            case 'Exit':
+                exit();
+                break;
+        }
+    });
+}
 
-    //Switch to get the user response and call the appropriate 
-    switch(response.input){
-        case 'View Products for Sale':
-            products();
-            break;
-        case 'View Low Inventory':
-            lowInventory();
-            break;
-        case 'Add to Inventory':
-            addInventory();
-            break;
-        case 'Add New Product':
-            addProduct();
-            break;
-    }
-});
 
 //  * If a manager selects `View Products for Sale`, the app should list every available item: the item IDs, names, prices, and quantities.
 function products(){
+    console.log("=============== AVAILABLE PRODUCTS ===============");
     var query = "SELECT * FROM products";
     connection.query(query, function(err, res){
         if(err) throw err;
@@ -50,29 +58,57 @@ function products(){
             console.log("-------------------------------------------------");
             console.log("| ID:\t" + res[ele].item_id + "\t || Name:\t" + res[ele].product_name + "\n| Price: $" + res[ele].price + "\t || Quantity:\t" + res[ele].stock_quantity + "");    
         }
-        connection.end();
+        //connection.end();
+        menu();
     });
 }
 
 //  * If a manager selects `View Low Inventory`, then it should list all items with an inventory count lower than five.
 function lowInventory(){
+    console.log("=============== LOW INVENTORY ===============");
     var query = ("SELECT * FROM products WHERE stock_quantity BETWEEN 0 AND 5");
     connection.query(query, function(err, res){
         if(err) throw err;
         //console.log(res);
         console.log("-------------------------------------------------");
         console.log("| ID:\t" + res[0].item_id + "\t || Name:\t" + res[0].product_name +"\n| Price: $" + res[0].price + "\t || Quantity:\t" + res[0].stock_quantity)
-        connection.end();
+        //connection.end();
+        menu();
     });
 }
 
 //  * If a manager selects `Add to Inventory`, your app should display a prompt that will let the manager "add more" of any item currently in the store.
 function addInventory(){
-    
+    console.log("=============== ADD NEW INVENTORY ===============");
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: 'What product are you adding inventory too? (ID) ',
+            name: 'id'
+        },{
+            type: 'input',
+            message: 'How much product are you adding? ',
+            name: 'newProduct'
+        }
+    ]).then(function(res){
+        var query = "SELECT * FROM products WHERE ?"
+        connection.query(query, {item_id: res.id}, function(err, response){
+            if(err) throw err;
+            var newProductTotal = parseInt(res.newProduct) + response[0].stock_quantity;
+            var query = "UPDATE products SET ? WHERE ?";
+            connection.query(query, [{stock_quantity: newProductTotal},{item_id: res.id}], function(err, response){
+                if(err) throw err;
+                console.log(response.affectedRows + " products updated!\n");
+            });
+            //connection.end();
+            menu();
+        })
+    })
 }
 
 //  * If a manager selects `Add New Product`, it should allow the manager to add a completely new product to the store.
 function addProduct(){
+    console.log("=============== ADD NEW PRODUCT ===============");
     inquirer.prompt([
         {
             type:"input",
@@ -101,8 +137,14 @@ function addProduct(){
         connection.query(query, function(err, res){
             if(err) throw err;
             console.log(res);
-            connection.end();
+            //connection.end();
+            menu();
         })
     })
     
+}
+
+function exit(){
+    console.log("=============== EXITING ===============");
+    connection.end();
 }
